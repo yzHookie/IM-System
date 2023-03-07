f65a796aac5846f27e8dbe6881eecf60c0354a37
yzHookie <126656963+yzHookie@users.noreply.github.com>
2023-03-07 15:48:39 +0800

    V0.3

---
+++ server.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"sync"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+
+	//在线用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	//消息广播的channel
+	Message chan string
+}
+
+// 创建一个server的API
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
+	return server
+}
+
+// 广播消息的方法
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+}
+
+// 监听Message广播消息的goroutine，一旦有消息就发给全部的在线user
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+
+		//将msg发给全部在线user
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+func (this *Server) Handler(conn net.Conn) {
+	//当前连接的业务
+	//fmt.Println("连接建立成功")
+	user := NewUser(conn)
+
+	//用户上线，将用户加入OnlineMap中
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlo()ck()
+
+	//广播当前用户上线信息
+	this.BroadCast(user, "已上线")
+
+	//接受客户端传的消息并广播
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				this.BroadCast(user, "下线")
+				return
+			}
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err:", err)
+			}
+			
+			msg := string(buf[ : n - 1])
+			this.BroadCast(user, msg)
+		}
+	}()
+	//当前handler阻塞
+	select {}
+}
+
+// 启动服务器的接口
+func (this *Server) Start() {
+	//sorket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+	//close listen socket
+	defer listener.Close()
+
+	//启动监听Message的goruntine
+	go this.ListenMessager()
+
+	for {
+		//accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept err", err)
+			continue
+		}
+
+		//do handler
+		go this.Handler(conn)
+	}
+}
